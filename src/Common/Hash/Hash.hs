0864b9c477872e7bc4723c0bf358cf67f7d91cfb
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-03 08:58:20 -0400

    src/Common/Hash: Create to string helper.

--- src/Common/Hash/Hash.hs
+++ src/Common/Hash/Hash.hs
@@ -1,10 +1,15 @@
-module Common.Hash where
+module Common.Hash.Hash where
 
 import qualified Crypto.Hash.BLAKE2.BLAKE2s    as Blake2
 import qualified Data.ByteString               as BS
+import           Data.ByteString.Base16        as Hex
+import           Data.Text.Encoding            as Encoding
 
 ----------------------------------------------------------------------------
 -- Hash
 ----------------------------------------------------------------------------
 
 newtype Hash = Hash BS.ByteString
+
+string :: Hash -> String
+string hash = Hex.encode hash
