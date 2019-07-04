ce510cd7137b9cc8fa8d625b4d38b82355e0ac54
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-04 09:08:21 -0400

    src/common/hash: Finalize hash helpers & types.

--- src/Common/Hash/Hash.hs
+++ src/Common/Hash/Hash.hs
@@ -1,15 +1,25 @@
-module Common.Hash.Hash where
+module Common.Hash.Hash
+    ( Hash
+    , hashBlake2
+    )
+where
 
 import qualified Crypto.Hash.BLAKE2.BLAKE2s    as Blake2
 import qualified Data.ByteString               as BS
 import           Data.ByteString.Base16        as Hex
+import           Data.Text                     as Text
 import           Data.Text.Encoding            as Encoding
 
 ----------------------------------------------------------------------------
 -- Hash
 ----------------------------------------------------------------------------
 
-newtype Hash = Hash BS.ByteString
+type Hash = BS.ByteString
 
-string :: Hash -> String
-string hash = Hex.encode hash
+-- Convert a given hash to a human-readable string
+string :: Hash -> Text.Text
+string hash = Encoding.decodeUtf8 (Hex.encode hash)
+
+-- Hash a given input, b, via blake2
+hashBlake2 :: BS.ByteString -> Hash
+hashBlake2 = Blake2.hash 32 mempty
